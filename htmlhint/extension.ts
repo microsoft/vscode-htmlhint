04439cb5e1c73f98712d5b6b9e0922dd2d3efa27
Mike Kaufman <mkaufman@microsoft.com>
2015-12-02 08:43:21 -0800

    fixed bad URL in package.json.  Cleaned up some whitespace formatting

--- htmlhint/extension.ts
+++ htmlhint/extension.ts
@@ -4,24 +4,24 @@ import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions } 
 
 export function activate(context: ExtensionContext) {
 
-	// We need to go one level up since an extension compile the js code into
-	// the output folder.
-	let serverModulePath = path.join(__dirname, '..', 'server', 'server.js');
-	let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
-	let serverOptions: ServerOptions = {
-		run: { module: serverModulePath },
-		debug: { module: serverModulePath, options: debugOptions}
-	};
+    // We need to go one level up since an extension compile the js code into
+    // the output folder.
+    let serverModulePath = path.join(__dirname, '..', 'server', 'server.js');
+    let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
+    let serverOptions: ServerOptions = {
+        run: { module: serverModulePath },
+        debug: { module: serverModulePath, options: debugOptions }
+    };
 
-	let clientOptions: LanguageClientOptions = {
-		documentSelector: ['html', 'htm'],
-		synchronize: {
-			configurationSection: 'htmlhint',
-			fileEvents: workspace.createFileSystemWatcher('**/httmlhint.json')
-		}
-	}
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['html', 'htm'],
+        synchronize: {
+            configurationSection: 'htmlhint',
+            fileEvents: workspace.createFileSystemWatcher('**/httmlhint.json')
+        }
+    }
 
     let forceDebug = false;
-	let client = new LanguageClient('HTML-hint', serverOptions, clientOptions, forceDebug);
-	context.subscriptions.push(new SettingMonitor(client, 'htmlhint.enable').start());
+    let client = new LanguageClient('HTML-hint', serverOptions, clientOptions, forceDebug);
+    context.subscriptions.push(new SettingMonitor(client, 'htmlhint.enable').start());
 }
